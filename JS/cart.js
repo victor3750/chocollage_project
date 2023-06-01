2df98a4f8d5a2f977cb53a3bfc68fc60ce81bd6c
Victor <vic.vic86@hotmail.com>
2023-06-01 16:53:42 +0800

    輪播 bootstrap套用新版本

--- JS/cart.js
+++ JS/cart.js
@@ -1,3 +1,4 @@
+// 數量增減
 $(function () {
     // This button will increment the value
     $('.qtyplus').click(function (e) {
@@ -49,3 +50,23 @@ $(function () {
         $(".contents-row").slideToggle('slow');
     });
 });
+
+const priceList = {
+    "經典緞帶-四入禮盒": 240,
+    "經典緞帶-六入禮盒": 360,
+    "經典緞帶-八入禮盒": 480,
+    "經典緞帶-十二入禮盒": 720,
+    "經典緞帶-十六入禮盒": 960
+};
+
+const quantityInput = document.querySelector('.qty');
+const priceDisplay = document.querySelector('.price');
+
+quantityInput.addEventListener('input', () => {
+    const selectedProduct = document.querySelector('.item').textContent.trim();
+    const quantity = parseInt(quantityInput.value);
+    const price = priceList[selectedProduct] * quantity;
+    priceDisplay.textContent = `$${price}`;
+});
+
+
