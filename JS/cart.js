30aaab21e3fc2c592c94051a4c257844a6a42711
Victor <vic.vic86@hotmail.com>
2023-06-02 01:53:43 +0800

    購物車新增功能（未完成）

--- JS/cart.js
+++ JS/cart.js
@@ -1,40 +1,64 @@
+// input只能輸入數字
+$('input[name=quantity]').on('input', function () {
+    this.value = this.value.replace(/[^0-9]/g, '');
+});
+// 價格隨數量改變
+const quantityInputs = document.querySelectorAll('[class^="list_"] .qty');
+
+quantityInputs.forEach(quantityInput => {
+    quantityInput.addEventListener('input', () => {
+        const selectedProduct = quantityInput.closest('tr').querySelector('.item').textContent.trim();
+        const quantity = parseInt(quantityInput.value) || 0;
+        const priceDisplay = quantityInput.closest('tr').querySelector('.price');
+        const price = priceList[selectedProduct] * quantity;
+        priceDisplay.textContent = `$${price || 0}`;
+    });
+});
 // 數量增減
 $(function () {
     // This button will increment the value
-    $('.qtyplus').click(function (e) {
+    $('[class^="list_"] .qtyplus').click(function (e) {
         // Stop acting like a button
         e.preventDefault();
-        // Get the field name
-        fieldName = $(this).attr('field');
+        // Get the parent row of the clicked button
+        var parentRow = $(this).closest('tr');
+        // Get the field name within the parent row
+        var fieldName = $(this).attr('field');
         // Get its current value
-        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
+        var currentVal = parseInt(parentRow.find('input[name=' + fieldName + ']').val());
         // If is not undefined
         if (!isNaN(currentVal)) {
             // Increment
-            $('input[name=' + fieldName + ']').val(currentVal + 1);
+            parentRow.find('input[name=' + fieldName + ']').val(currentVal + 1);
+            
         } else {
             // Otherwise put a 0 there
-            $('input[name=' + fieldName + ']').val(0);
+            parentRow.find('input[name=' + fieldName + ']').val(0);
         }
     });
+
     // This button will decrement the value till 0
-    $(".qtyminus").click(function (e) {
+    $('[class^="list_"] .qtyminus').click(function (e) {
         // Stop acting like a button
         e.preventDefault();
-        // Get the field name
-        fieldName = $(this).attr('field');
+        // Get the parent row of the clicked button
+        var parentRow = $(this).closest('tr');
+        // Get the field name within the parent row
+        var fieldName = $(this).attr('field');
         // Get its current value
-        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-        // If it isn't undefined or its greater than 0
+        var currentVal = parseInt(parentRow.find('input[name=' + fieldName + ']').val());
+        // If it isn't undefined or it's greater than 0
         if (!isNaN(currentVal) && currentVal > 0) {
             // Decrement one
-            $('input[name=' + fieldName + ']').val(currentVal - 1);
+            parentRow.find('input[name=' + fieldName + ']').val(currentVal - 1);
         } else {
             // Otherwise put a 0 there
-            $('input[name=' + fieldName + ']').val(0);
+            parentRow.find('input[name=' + fieldName + ']').val(0);
         }
     });
 });
+
+
 
 // 購物車展開收合鍵
 $(function () {
@@ -58,15 +82,3 @@ const priceList = {
     "經典緞帶-十二入禮盒": 720,
     "經典緞帶-十六入禮盒": 960
 };
-
-const quantityInput = document.querySelector('.qty');
-const priceDisplay = document.querySelector('.price');
-
-quantityInput.addEventListener('input', () => {
-    const selectedProduct = document.querySelector('.item').textContent.trim();
-    const quantity = parseInt(quantityInput.value);
-    const price = priceList[selectedProduct] * quantity;
-    priceDisplay.textContent = `$${price}`;
-});
-
-
