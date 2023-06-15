bf7a5faf0328f01a1ed49f846e9ef6a9c179846a
Victor <vic.vic86@hotmail.com>
2023-06-15 18:03:42 +0800

    訂單確認(RWD),首頁TOP鍵

--- JS/hamburger.js
+++ JS/hamburger.js
@@ -4,4 +4,16 @@ $(document).ready(function () {
         $(this).toggleClass('is-active');
         $('.navigation').toggleClass('show');
     });
+    // 滑動至頂
+    $('#gotop').click(function () {
+        $("html,body").animate({ scrollTop: 0 }, 300);
+    });
+    // 指定捲軸位置淡出淡入
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('#gotop').stop().fadeIn('fast');
+        } else {
+            $('#gotop').stop().fadeOut('fast');
+        }
+    });
 });
