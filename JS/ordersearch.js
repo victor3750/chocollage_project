7b2e226937d91cc0a1605053cd8797cc6a9a63c6
Victor <vic.vic86@hotmail.com>
2023-06-26 02:36:31 +0800

    避搜尋，訂單查詢修改
    訂單查詢頁面可同時存在多項訂單

--- JS/ordersearch.js
+++ JS/ordersearch.js
@@ -1,14 +1,29 @@
-// 展開收合鍵
+// // 展開收合鍵
+// $(function () {
+//     $('.expand').click(function () {
+//         var img = $(this).find('img');
+//         if (img.attr('src') === '/images/ordersearch/ARROW_expand.png') {
+//             img.attr('src', '/images/ordersearch/ARROW_close.png');
+//             img.attr('alt', '收合');
+//         } else {
+//             img.attr('src', '/images/ordersearch/ARROW_expand.png');
+//             img.attr('alt', '展開');
+//         }
+//         $(".dragform").slideToggle('slow');
+//     });
+// });
+
 $(function () {
     $('.expand').click(function () {
         var img = $(this).find('img');
-        if (img.attr('src') === '/images/ordersearch/ARROW_expand.png') {
-            img.attr('src', '/images/ordersearch/ARROW_close.png');
+        if (img.attr('src') === 'images/cart/expand.svg') {
+            img.attr('src', 'images/cart/close.svg');
             img.attr('alt', '收合');
         } else {
-            img.attr('src', '/images/ordersearch/ARROW_expand.png');
+            img.attr('src', 'images/cart/expand.svg');
             img.attr('alt', '展開');
         }
-        $(".dragform").slideToggle('slow');
+        var listNumber = $(this).closest('tr').attr('class').match(/list_(\d+)/)[1];
+        $(".contents-row_" + listNumber).slideToggle('slow');
     });
 });
